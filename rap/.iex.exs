5c7edec3bd58a3b5ec8300263d18a35e71c7613d
dguthrie <dguthrie@posteo.net>
2024-04-02 17:29:43 +0000

    mse -> mae (mean absolute error)
    FossilOrigin-Name: fff025414c23f3a7e48bcc9742c0a318f9e08782ff7877b328cddc54613e893e

--- rap/.iex.exs
+++ rap/.iex.exs
@@ -4,6 +4,10 @@ k2 = 3..22 |> Enum.into([])
 k3 = 0..100 |> Enum.into([])
 k4 = 20..120 |> Enum.into([])
 
-job0 = { :job_dummy, k0, k1 }
-job1 = { :job_dummy, k1, k2 }
-job2 = { :job_dummy, k3, k4 }
+job0dummy = { :job_dummy, k0, k1 }
+job1dummy = { :job_dummy, k1, k2 }
+job2dummy = { :job_dummy, k3, k4 }
+
+job0mae = { :job_mae, k0, k1 }
+job1mae = { :job_mae, k1, k2 }
+job2mae = { :job_mae, k3, k4 }
--- rap/lib/aux/jobs.ex
+++ rap/lib/aux/jobs.ex
@@ -4,7 +4,7 @@ defmodule RAP.Job.Spec do
   require Logger
   
   def run_job({ :job_dummy, col0, col1 }), do: dummy(col0, col1)
-  def run_job({ :job_mse, col0, col1 }), do: mse(col0, col1)
+  def run_job({ :job_mae, col0, col1 }), do: mae(col0, col1)
   def run_job({ :job_emd, col0, col1 }), do: emd(col0, col1)
   
   defp dummy col0, col1 do
@@ -15,7 +15,22 @@ defmodule RAP.Job.Spec do
     res
   end
   
-  defp mse(_col0, _col1), do: :ok
+  defp mae_verbose col0, col1 do
+
+    tuples = Enum.zip(col0, col1)
+    errors = Enum.map(tuples, fn {y, x} -> abs(y - x) end)
+    mae    = Math.Enum.mean(errors)
+    Logger.info "Result of Job.Spec.mae: #{inspect mae}"
+    mae
+  end
+
+  defp mae col0, col1 do
+    Logger.info "Called Job.Spec.mae (col0 = #{inspect col0} ,col1 = #{inspect col1})"
+    Enum.zip( col0, col1)
+    |> Enum.map( fn{y, x} -> abs(y - x) end)
+    |> Math.Enum.mean()
+  end
+    
   defp emd(_col0, _col1), do: :ok
   
 end
