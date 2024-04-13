14df9ab6a9afdc136448fde623f2e53fa206a23e
dguthrie <dguthrie@posteo.net>
2024-04-13 14:43:40 +0000

    Get RDF import working with RDF.ex and Grax
    This involved a few changes:
    1. Update the data model to specify an identifier for each class.
       (This was accompanied by a rather more annoying interaction between
       LinkML and RDFLib, which I am not sure how to debug; it generates
       a spurrious @prefix @base line which trips up both RDFLib and
       RDF.ex.)
    2. Rebulid the data model's RDF description and import it here. I had
       previously generated turtle. This isn't a problem, but when I was
       debugging the RDF imports, since the other vocabularies cribbed from
       the outdated package are n-triples. So, we might as well leave it as
       n-triples and remove the turtle file.
    3. Make `fisdat'/`fisjob' &c. generate RDF with identifiers
    4. In this Elixir demo RAP, for each manifest component description, use
       Grax with `depth: +2', otherwise any source descriptions embedded in
       job descriptions were not expanded.
    
    Sample code in iex(1), which injects into all fields correctly:
    
    % { :ok, graph }  = RDF.Turtle.read_file "manual_test/manifest0.ttl"
    % { :ok, egraph } = Grax.load graph, RAP.Vocabularies.SAVED.RootManifest, RAP.Manifest.ManifestDesc
    
    FossilOrigin-Name: fbe18ed1b68b2f1fe3599748bf455de83a905937ed8a0ad1c38f604eb2269ea4

--- rap/lib/manifest/manifest.ex
+++ rap/lib/manifest/manifest.ex
@@ -1,13 +1,56 @@
-defmodule RAP.Job.Manifest.TableDesc do
+defmodule RAP.Manifest.TableDesc do
   
-  use Grax.Schema
+  use Grax.Schema, depth: +2
   import RDF.Sigils
-
-  alias RAP.Vocabularies.{DCAT,DCTERMS,DCE,Schema,LinkML,SAVED}
+  alias RAP.Vocabularies.{DCTERMS,SAVED}
   
   schema SAVED.TableDesc do
-    property :title, DCTERMS.title
-    property :atomic_name, SAVED.atomic_name
-    property :job_auto_generate, SAVED.job_auto_generate
+    property :title,         DCTERMS.title,       type: :string
+    property :description,   DCTERMS.description, type: :string
+    property :resource_path, SAVED.resource_path, type: :string
+    property :schema_path,   SAVED.schema_path,   type: :string
+    property :resource_hash, SAVED.resource_hash, type: :string
+    property :scope,         SAVED.scope,         type: list_of(:string)
+  end
+end
+
+defmodule RAP.Manifest.SourceDesc do
+
+  use Grax.Schema, depth: +2
+  import RDF.Sigils
+  alias RAP.Vocabularies.SAVED
+
+  schema SAVED.SourceDesc do
+    property :scope, SAVED.scope, type: list_of(:string)
+  end
+end
+
+defmodule RAP.Manifest.JobDesc do
+
+  use Grax.Schema, depth: +2
+  import RDF.Sigils
+  alias RAP.Vocabularies.{DCTERMS,SAVED}
+  alias RAP.Manifest.{SourceDesc}
+
+  schema SAVED.JobDesc do
+    property :title,             DCTERMS.title,           type: :string
+    property :job_type,          SAVED.job_type,          type: :string
+    property :job_auto_generate, SAVED.job_auto_generate, type: :boolean
+
+    link job_sources: SAVED.job_sources, type: list_of(RAP.Manifest.SourceDesc)
+  end
+end
+
+defmodule RAP.Manifest.ManifestDesc do
+
+  use Grax.Schema, depth: +2
+  import RDF.Sigils
+  alias RAP.Vocabularies.SAVED
+
+  schema SAVED.ManifestDesc do
+    property :local_version, SAVED.local_version
+
+    link tables: SAVED.tables, type: list_of(RAP.Manifest.TableDesc)
+    link jobs:   SAVED.jobs,   type: list_of(RAP.Manifest.JobDesc)
   end
 end
--- rap/lib/manifest/vocabularies.ex
+++ rap/lib/manifest/vocabularies.ex
@@ -62,9 +62,9 @@ defmodule RAP.Vocabularies do
     case_violations: :ignore
 
   @vocabdoc """
-  The LinkML specification
+  The LinkML meta-model
 
-  See <https://w3id.org/linkml/>
+  See <https://w3id.org/linkml/meta>
   """
   defvocab LinkML,
     base_iri: "https://w3id.org/linkml/",
@@ -74,17 +74,61 @@ defmodule RAP.Vocabularies do
       linkml_alias: "alias"
     ]
 
+  #@vocabdoc """
+  #Addendum to the LinkML meta-model: data-sets
+  #
+  #See <https://w3id.org/linkml/datasets>
+  #"""
+  #defvocab LinkMLDataSets,
+  #  base_iri: "https://w3id.org/linkml/datasets/",
+  #  file: "linkml_datasets.ttl",
+  #  case_violations: :ignore
+
   @vocabdoc """
   SAVED data-model
-
+  
   Temporarily living on localhost.
-
+  
   See <https://github.com/saved-models/data-model>
+
+  Note that the meta-model imports *most* of schemata in its repository,
+  but not all, e.g. it doesn't import `datasets.yaml'. The two
+  identifiers we're actually interested here are `path' and `hash'.
+
+  It's fair to say that the utility in importing the main LinkML
+  meta-model as above is in being able to link both to LinkML proper and
+  the links to other schemata and data models which it defines / imports,
+  so given that it apparently does not import that component, just refer
+  to these in our own schema.
+
+  A further element of this is that it isn't actually clear why/how
+  LinkML does this, nor how they generated their initial schemata/data
+  model. Answering this is probably pertinent to deciding how to define
+  the equivalent Grax schema/struct into which our RDF job manifests are
+  loaded/injected.
   """
   defvocab SAVED,
     base_iri: "http://localhost/saved/",
-    file: "saved.ttl",
+    file: "saved.nt",
     case_violations: :ignore,
-    terms: {RAP.TermHandler, :sub_special, [:variant1]}
+    alias: [
+      JSON_LD: "JSON-LD",
+      LD_Patch: "LD%20Patch",
+      N_Quads: "N-Quads",
+      N_Triples: "N-Triples",
+      OWL_Functional_Syntax: "OWL%20Functional%20Syntax",
+      OWL_Manchester_Syntax: "OWL%20Manchester%20Syntax",
+      OWL_XML_Serialisation: "OWL%20XML%20Serialization",
+      POWDER_S: "POWDER-S",
+      PROV_N: "PROV-N", "PROV_XML": "PROV-XML",
+      RIF_XML_Syntax: "RIF%20XML%20Syntax",
+      SPARQL_Results_in_CSV: "SPARQL%20Results%20in%20CSV",
+      SPARQL_Results_in_JSON: "SPARQL%20Results%20in%20JSON",
+      SPARQL_Results_in_TSV: "SPARQL%20Results%20in%20TSV",
+      SPARQL_Results_in_XML: "SPARQL%20Results%20in%20XML",
+      RDF_XML: "rdf-xml"    
+    ],
+    strict: false  
+    #terms: {RAP.TermHandler, :sub_special, [:variant1]}
 
 end
