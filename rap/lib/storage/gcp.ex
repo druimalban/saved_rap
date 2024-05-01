f255ce3a08585a4f55948be5c14737db6dbbff2b
dguthrie <dguthrie@posteo.net>
2024-05-01 21:47:48 +0000

    Initial steps to fetch data from GCP
    This is largely the right shape, but it will be important to nail down
    the named struct for the initial GCP storage steps, and make sure that
    the file names and such get passed all the way through to the cache /
    'bakery' steps (don't rely on the manifest descriptions of files where
    possible).
    
    FossilOrigin-Name: 178e70cbc2b6413f3145edacdeb7f6c6be716b0dd489993ecb6e2bae5e1b5262

--- rap/lib/storage/gcp.ex
+++ rap/lib/storage/gcp.ex
@@ -10,6 +10,9 @@ defmodule RAP.Storage.GCP do
   alias GoogleApi.Storage.V1.Api.Objects,   as: GCPReqObjs
 
   @gcp_scope "https://www.googleapis.com/auth/cloud-platform"
+  @interval_seconds 300
+  @cache_dir "./data_cache"
+  @manifest "manifest.ttl"
 
   def start_link initial_ts do
     Logger.info "Called Storage.GCP.start_link (initial_ts = #{inspect initial_ts})"
@@ -26,27 +29,36 @@ defmodule RAP.Storage.GCP do
     insd = inspect demand
     inss = inspect ts
     events = []
-    Logger.info "Called Job.Producer.handle_demand (demand = #{insd}, state = #{inss})"
+    Logger.info "Called `Storage.GCP.handle_demand (demand = #{insd}, state = #{inss})'"
     { :noreply, events, ts }
+  end
+
+  def new_connection() do
+    with {:ok, token} <- Goth.Token.for_scope(@gcp_scope),
+         session      <- GCPConn.new(token.token) do
+      Logger.info "Called Storage.GCP.new_connection/0"
+      {:ok, session}
+    else
+      :error -> {:error, "Cannot obtain token/session" }
+    end
   end
   
   @doc """
   Trigger the monitoring of a given GCP bucket
   """
-  def watch_gcp(bucket_name) do
-    Logger.info("Call `Producer.watch_gcp (bucket: #{bucket_name})'")
-    with {:ok, token} <- Goth.Token.for_scope(@gcp_scope),
-	 session      <- GCPConn.new(token.token)
+  def watch_bucket(bucket_name) do
+    Logger.info("Call `Storage.GCP.watch_gcp (bucket: #{bucket_name})'")
+    with {:ok, session} <- new_connection()
       do
       initial_ts = DateTime.utc_now() |> DateTime.to_unix()
       monitor_session(session, bucket_name, initial_ts, initial: true)
     else
-      :error -> {:error, "Cannot obtain token"}
+      { :error, _ } = err -> err
     end
   end
 
   def features_helper(%GCPObj{} = obj) do
-    Logger.info "Called `features_helper' on object #{obj.name}"
+    Logger.info "Called `Storage.GCP.features_helper' on object #{obj.name}"
      %{id:         obj.id,
        bucket:     obj.bucket,
        md5:        obj.md5Hash,
@@ -70,7 +82,7 @@ defmodule RAP.Storage.GCP do
   files.
   """
   def uuid_helper(gcp_object) do
-    Logger.info "Called `uuid_helper' on object #{gcp_object.name}"
+    Logger.info "Called `Storage.GCP.uuid_helper' on object #{gcp_object.name}"
     with [owner, _date, uuid, res | k] <- String.split(gcp_object.name, "/"),
 	 is_directory <- length(k) > 0
     do
@@ -79,7 +91,7 @@ defmodule RAP.Storage.GCP do
 	%{owner: owner, uuid: uuid, file: res, dir: is_directory}
       )
     else
-      [x] -> {:error, gcp_object}
+      [_|_] -> nil
     end
   end
 
@@ -90,7 +102,7 @@ defmodule RAP.Storage.GCP do
     case :ets.lookup(:uuid, uuid) do
       [] -> true
       _  ->
-	Logger.info("Found job UUID #{uuid} is running!")
+	Logger.info("Job UUID #{uuid} is already running, cannot add to ETS.")
 	false
     end
   end
@@ -113,26 +125,62 @@ defmodule RAP.Storage.GCP do
   """
   def monitor_session(session, bucket_name, ts, initial \\ false) do
     if initial do
-      Logger.info "Initial call to monitor_session (_session, bucket: #{bucket_name}, time stamp: #{inspect ts})"
+      Logger.info "Initial call to `GCP.Storage.monitor_session' with interval #{inspect @interval_seconds}s"
     end
     curr_ts = DateTime.utc_now() |> DateTime.to_unix
-    if (curr_ts - ts) < 300 and !initial do
+    if (curr_ts - ts) < @interval_seconds and !initial do
       monitor_session(session, bucket_name, ts)
     else
       with {:ok, %GCPObjs{items: objects}} <- GCPReqObjs.storage_objects_list(session, bucket_name)
-      do
+	do
 	objects
 	|> Enum.map(&features_helper/1)
 	|> Enum.map(&uuid_helper/1)
-	|> then(&GenStage.cast(__MODULE__, {:check_then_run, &1}))
+	|> then(&GenStage.cast(__MODULE__, {:check_then_run, session, bucket_name, &1}))
 	
 	new_ts = DateTime.utc_now() |> DateTime.to_unix
 	monitor_session(session, bucket_name, new_ts)
       else
-	{:error, error = %Tesla.Env{status: code, url: uri, body: msg}} ->
+	{:error, error = %Tesla.Env{status: 401}} ->
+	  Logger.info "Query of GCP bucket #{bucket_name} appeared to time out, seek new session"
+	  # If this bit fails, we know there's really something up!
+	  {:ok, new_session} = new_connection()
+	  monitor_session(new_session, bucket_name, ts) 
+        {:error, error = %Tesla.Env{status: code, url: uri, body: msg}} ->
 	  Logger.info "Query of GCP failed with code #{code} and error message #{msg}"
 	  {:error, uri, code, msg}
       end
+    end
+  end
+
+  @doc """
+  Actually fetch the objects in question
+
+  The data cache directory effectively mirrors the GCP object store's
+  purported structure but we have the benefit of the mnesia database to
+  cache results.
+  """
+  def fetch_job_deps(session, bucket_name, obj) do
+    Logger.info "Called `Storage.GCP.fetch_uuid_deps' for job with UUID #{obj.uuid}"
+    with {:ok, %Tesla.Env{body: body, status: 200}} <- GCPReqObjs.storage_objects_get(session, bucket_name, obj.name, [alt: "media"], decode: false),
+         target_dir  <- "#{@cache_dir}/#{obj.owner}/#{obj.uuid}",
+	 target_file <- "#{target_dir}/#{obj.file}",
+         :ok <- File.mkdir_p(target_dir),
+         :ok <- File.write(target_file, body)
+      do
+        Logger.info "`Storage.GCP.fetch_job_deps/3': Successfully wrote #{target_file}"
+        target_file
+      else
+	{:error, error = %Tesla.Env{status: 401}} ->
+	  Logger.info "Query of GCP bucket #{bucket_name} appeared to time out, seek new session"
+	  # If this bit fails, we know there's really something up!
+	  {:ok, new_session} = new_connection()
+	  fetch_job_deps(new_session, bucket_name, obj)
+	{:error, error = %Tesla.Env{status: code, url: uri, body: msg}} ->
+	  Logger.info "Query of GCP failed with code #{code} and error message #{msg}"
+	  {:error, uri, code, msg}
+	{:error, raison } ->
+	  Logger.info "Error creating file/directory due to #{raison}"
     end
   end
 
@@ -163,26 +211,37 @@ defmodule RAP.Storage.GCP do
   the only normalisation that need take place, because as it currently
   stands, the manifest refers to all of the dependent files.
   """
-  def handle_cast({:check_then_run, objs}, state) do
-    Logger.info "Received cast :check_then_run"
-    uncached_jobs = objs
+  def handle_cast({:check_then_run, session, bucket_name, objs}, state) do
+    Logger.info "`Storage.GCP' received `:check_then_run' cast"
+    
+    remote_uuids = objs
+    |> Enum.reject(&is_nil/1)
     |> Enum.map(& &1.uuid)
     |> Enum.uniq()
+    Logger.info("Found unique UUIDs on GCP: #{inspect remote_uuids}")
+    
+    staging_uuids = remote_uuids
     |> Enum.filter(&Transactions.feasible?/1)
     |> Enum.filter(&ets_feasible?/1)
     
     grouped_file_objs = objs
+    |> Enum.reject(&is_nil/1)
     |> Enum.filter(&  !&1.dir)
     |> Enum.group_by(& &1.uuid)
 
-    insert_uuid = fn(uuid) ->
+    run_job = fn (uuid) ->
       fps = grouped_file_objs |> Map.get(uuid) 
       Logger.info "#{uuid} => #{inspect fps}"
-      :ets.insert({uuid})
+      :ets.insert(:uuid, {uuid})
       Logger.info("Inserted #{uuid} into Erlang term storage (:ets)")
+
+      # session, bucket_name, obj
+      fps |> Enum.each(& fetch_job_deps(session, bucket_name, &1))
     end
-    uncached_jobs
-    |> Enum.each(insert_uuid)
+
+    # Insert the running UUIDs into ETS
+    staging_uuids
+    |> Enum.each(run_job)
     
     {:noreply, [], state} # Don't yet produce events
   end
