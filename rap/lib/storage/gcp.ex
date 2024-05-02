2cf1806b3c77386ed550217e08c371397a26bf92
dguthrie <dguthrie@posteo.net>
2024-05-02 14:49:49 +0000

    Start separating the monitoring of storage from the polling of it
    The idea here is that the producer stage simply produces descriptions of
    jobs which are to be run, and the source of them. This separates regular
    polling of various storage backends from the polling proper.
    
    This is advantageous in several respects:
    
    1. Polling just for a list of UUIDs is not expensive computationally and
       and should be separated from the various recursive functions which
       fetch the data proper, which may be computationally expensive and may
       imply race conditions and/or file conflicts.
    
    2. This will put the over-complicated fetching of objects into separate
       module stages which depend on this original producer, and apparently
       to use `ConsumerSupervisor' or similar to parallelise it.
    
    3. The logic which checks that a UUID is not running nor has a cached
       result is really related to producing events, so it's best to locate
       it in the producer stage.
    
    There's a lot involved in separating these, so commit this code as-is
    to keep track of changes as I go along. The previous working version did
    was not yet joined up with the rest of the stages, anyway, so this is the
    ideal time at which to start separating them.
    
    FossilOrigin-Name: 6ae373ad63c5cb255639e68cbeef48ba50195c2fcadde2b38138d4b650f81004

--- rap/lib/storage/gcp.ex
+++ rap/lib/storage/gcp.ex
@@ -1,158 +1,24 @@
 defmodule RAP.Storage.GCP do
+  @moduledoc """
+  This is the first stage of the pipeline, but the first stage could
+  easily be monitoring local storage or some other cloud storage thing.
 
+  The files are uploaded as if there's a directory structure, but there
+  isn't really because the GCP storage buckets are a flat object-based
+  structure. Thus associating and gathering files into jobs is somewhat
+  non-trivial and more involved than it should be.
+  """
   use GenStage
   require Logger
 
-  alias RAP.Storage.Transactions
   alias GoogleApi.Storage.V1.Connection,    as: GCPConn
   alias GoogleApi.Storage.V1.Model.Object,  as: GCPObj
   alias GoogleApi.Storage.V1.Model.Objects, as: GCPObjs
   alias GoogleApi.Storage.V1.Api.Objects,   as: GCPReqObjs
 
-  @gcp_scope "https://www.googleapis.com/auth/cloud-platform"
-  @interval_seconds 300
-  @cache_dir "./data_cache"
-  @manifest "manifest.ttl"
-
-  def start_link initial_ts do
-    Logger.info "Called Storage.GCP.start_link (initial_ts = #{inspect initial_ts})"
-    GenStage.start_link __MODULE__, initial_ts, name: __MODULE__
-  end
-
-  def init initial_ts do
-    Logger.info "Called Storage.GCP.init (initial_ts = #{inspect initial_ts})"
-    :ets.new(:uuid, [:set, :public, :named_table])
-    { :producer, initial_ts }
-  end
+  alias RAP.Storage.Monitor
+  alias RAP.Storage.Transactions
   
-  def handle_demand demand, ts do
-    insd = inspect demand
-    inss = inspect ts
-    events = []
-    Logger.info "Called `Storage.GCP.handle_demand (demand = #{insd}, state = #{inss})'"
-    { :noreply, events, ts }
-  end
-
-  def new_connection() do
-    with {:ok, token} <- Goth.Token.for_scope(@gcp_scope),
-         session      <- GCPConn.new(token.token) do
-      Logger.info "Called Storage.GCP.new_connection/0"
-      {:ok, session}
-    else
-      :error -> {:error, "Cannot obtain token/session" }
-    end
-  end
-  
-  @doc """
-  Trigger the monitoring of a given GCP bucket
-  """
-  def watch_bucket(bucket_name) do
-    Logger.info("Call `Storage.GCP.watch_gcp (bucket: #{bucket_name})'")
-    with {:ok, session} <- new_connection()
-      do
-      initial_ts = DateTime.utc_now() |> DateTime.to_unix()
-      monitor_session(session, bucket_name, initial_ts, initial: true)
-    else
-      { :error, _ } = err -> err
-    end
-  end
-
-  def features_helper(%GCPObj{} = obj) do
-    Logger.info "Called `Storage.GCP.features_helper' on object #{obj.name}"
-     %{id:         obj.id,
-       bucket:     obj.bucket,
-       md5:        obj.md5Hash,
-       uri_media:  obj.mediaLink,
-       uri_self:   obj.selfLink,
-       ts_created: obj.timeCreated,
-       ts_updated: obj.updated,
-       mime_type:  obj.contentType,
-       name:       obj.name       }
-  end
-
-  @doc """
-  Given an object returned by GCP, the files of which are uploaded in the
-  following format, extract the useful parts from this well-known
-  object name.
-
-  The flat objects' names should be a string in the form
-  `<owner>/<yyyymmdd>/<uuid>/<file>', with or without a trailing slash.
-  The trailing slash conveniently tells us that it's a directory; objects
-  have `text/plain' for directories, `application/octet_stream' for empty
-  files.
-  """
-  def uuid_helper(gcp_object) do
-    Logger.info "Called `Storage.GCP.uuid_helper' on object #{gcp_object.name}"
-    with [owner, _date, uuid, res | k] <- String.split(gcp_object.name, "/"),
-	 is_directory <- length(k) > 0
-    do
-      gcp_object
-      |> Map.merge(
-	%{owner: owner, uuid: uuid, file: res, dir: is_directory}
-      )
-    else
-      [_|_] -> nil
-    end
-  end
-
-  @doc """
-  Simple wrapper around Erlang term storage table of UUIDs
-  """
-  defp ets_feasible?(uuid) do
-    case :ets.lookup(:uuid, uuid) do
-      [] -> true
-      _  ->
-	Logger.info("Job UUID #{uuid} is already running, cannot add to ETS.")
-	false
-    end
-  end
-
-  @doc """
-  Monitors the GCP objects every five minutes.
-
-  The Google GCP buckets have a fake directory structure: they're by
-  default stored in this flat object-based structure which the web
-  interface hides.
-
-  The API has several 'Folder' and 'ManagedFolder' functions but these
-  only work on buckets which have a 'hierarchical' namespace, and the
-  documentation is so bad that there does not appear to be a way to set
-  this on the web interface.
-
-  If the request is successful, cast to the check_then_run function
-  which will check the UUIDs against 1. completed jobs (an mnesia
-  database) and 2. jobs, if any, currently running.
-  """
-  def monitor_session(session, bucket_name, ts, initial \\ false) do
-    if initial do
-      Logger.info "Initial call to `GCP.Storage.monitor_session' with interval #{inspect @interval_seconds}s"
-    end
-    curr_ts = DateTime.utc_now() |> DateTime.to_unix
-    if (curr_ts - ts) < @interval_seconds and !initial do
-      monitor_session(session, bucket_name, ts)
-    else
-      with {:ok, %GCPObjs{items: objects}} <- GCPReqObjs.storage_objects_list(session, bucket_name)
-	do
-	objects
-	|> Enum.map(&features_helper/1)
-	|> Enum.map(&uuid_helper/1)
-	|> then(&GenStage.cast(__MODULE__, {:check_then_run, session, bucket_name, &1}))
-	
-	new_ts = DateTime.utc_now() |> DateTime.to_unix
-	monitor_session(session, bucket_name, new_ts)
-      else
-	{:error, error = %Tesla.Env{status: 401}} ->
-	  Logger.info "Query of GCP bucket #{bucket_name} appeared to time out, seek new session"
-	  # If this bit fails, we know there's really something up!
-	  {:ok, new_session} = new_connection()
-	  monitor_session(new_session, bucket_name, ts) 
-        {:error, error = %Tesla.Env{status: code, url: uri, body: msg}} ->
-	  Logger.info "Query of GCP failed with code #{code} and error message #{msg}"
-	  {:error, uri, code, msg}
-      end
-    end
-  end
-
   @doc """
   Actually fetch the objects in question
 
@@ -162,14 +28,16 @@ defmodule RAP.Storage.GCP do
   """
   def fetch_job_deps(session, bucket_name, obj) do
     Logger.info "Called `Storage.GCP.fetch_uuid_deps' for job with UUID #{obj.uuid}"
+    target_dir  = "#{@cache_dir}/#{obj.owner}/#{obj.uuid}"
+    target_file = "#{target_dir}/#{obj.file}"
     with {:ok, %Tesla.Env{body: body, status: 200}} <- GCPReqObjs.storage_objects_get(session, bucket_name, obj.name, [alt: "media"], decode: false),
-         target_dir  <- "#{@cache_dir}/#{obj.owner}/#{obj.uuid}",
-	 target_file <- "#{target_dir}/#{obj.file}",
-         :ok <- File.mkdir_p(target_dir),
-         :ok <- File.write(target_file, body)
+	 {:ok, "correct", _md5} <- dl_success?(obj, body),
+         false <- File.exists?(target_file),
+         :ok   <- File.mkdir_p(target_dir),
+         :ok   <- File.write(target_file, body)
       do
         Logger.info "`Storage.GCP.fetch_job_deps/3': Successfully wrote #{target_file}"
-        target_file
+        {:ok, target_file}
       else
 	{:error, error = %Tesla.Env{status: 401}} ->
 	  Logger.info "Query of GCP bucket #{bucket_name} appeared to time out, seek new session"
@@ -179,8 +47,16 @@ defmodule RAP.Storage.GCP do
 	{:error, error = %Tesla.Env{status: code, url: uri, body: msg}} ->
 	  Logger.info "Query of GCP failed with code #{code} and error message #{msg}"
 	  {:error, uri, code, msg}
+        {:error, "incorrect", md5} ->
+	  Logger.info "Downloaded file does not match purported checksum #{md5}!"
+	  {:error, md5}
+	true ->
+	  Logger.info "Target directory already exists, but job #{obj.uuid} is neither running nor has been completed"
+	  File.rm_rf(target_dir)
+	  fetch_job_deps(session, bucket_name, obj)
 	{:error, raison } ->
 	  Logger.info "Error creating file/directory due to #{raison}"
+	  {:error, target_dir}
     end
   end
 
@@ -229,19 +105,19 @@ defmodule RAP.Storage.GCP do
     |> Enum.filter(&  !&1.dir)
     |> Enum.group_by(& &1.uuid)
 
-    run_job = fn (uuid) ->
+    prep_job = fn (uuid) ->
       fps = grouped_file_objs |> Map.get(uuid) 
       Logger.info "#{uuid} => #{inspect fps}"
       :ets.insert(:uuid, {uuid})
       Logger.info("Inserted #{uuid} into Erlang term storage (:ets)")
-
-      # session, bucket_name, obj
-      fps |> Enum.each(& fetch_job_deps(session, bucket_name, &1))
+      fps |> Enum.each(&fetch_job_deps(session, bucket_name, &1))
     end
 
     # Insert the running UUIDs into ETS
     staging_uuids
-    |> Enum.each(run_job)
+    |> Enum.each(prep_job)
+
+    staging_uuids
     
     {:noreply, [], state} # Don't yet produce events
   end
--- rap/lib/storage/storage.ex
+++ rap/lib/storage/storage.ex
@@ -9,15 +9,14 @@ defmodule RAP.Storage do
   these fields (e.g. LinkML YAML schemata vs turtle equivalent). Make it
   work for one and then extend it.
   """
-  
   use Amnesia
 
   alias RAP.Job.Runner
 
-  defstruct [ :uuid, :resource_file, :resource_schema, :job_manifest ]
+  defstruct [ :uuid, :manifest, :resources, :results ]
 
   defdatabase DB do
-    deftable Job, [:uuid, :resource_file, :resource_schema, :job_manifest, :job_results], type: :bag
+    deftable Job, [:uuid, :manifest, :resources, :results], type: :bag
   end 
 end
 
@@ -38,7 +37,6 @@ defmodule RAP.Storage.Transactions do
       |> RAP.Storage.DB.Job.write()
     end
   end
-
   
   def feasible?(uuid) do
     Amnesia.transaction do
@@ -51,4 +49,208 @@ defmodule RAP.Storage.Transactions do
     end
   end
   
+end
+
+defmodule RAP.Storage.Monitor do
+  @moduledoc """
+  The idea is that this producer only finds UUIDs, and then produces
+  events which are used to fetch the data.
+
+  This locates separates the recursive checks from the actual disk usage,
+  since the concurrency matters more for fetching data, as there may be
+  race conditions and there's quite an extensive set of checks necessary
+  to fetch data in a way that doesn't produce race conditions or over-use
+  the network.
+
+  Additionally, there is a distinctive issue of having to renew GCP
+  authentication tokens / cookies. Locating this here with a bunch of
+  callbacks / IPC is more robust than really recursive functions.
+  """
+  use GenStage
+  require Logger
+
+  alias GoogleApi.Storage.V1.Connection,    as: GCPConn
+  alias GoogleApi.Storage.V1.Model.Object,  as: GCPObj
+  alias GoogleApi.Storage.V1.Model.Objects, as: GCPObjs
+  alias GoogleApi.Storage.V1.Api.Objects,   as: GCPReqObjs
+
+  alias RAP.Storage.Monitor
+  alias RAP.Storage.Transactions
+
+  # Hard-code these for brevity, should be configurable at program init.
+  # Same cache dir for both the 'local' store and GCP
+  @gcp_scope "https://www.googleapis.com/auth/cloud-platform"
+  @interval_seconds 300
+  @cache_dir "./data_cache"
+
+  # This is the one file name which must be hard-coded, because there's
+  # no way to know which file is which. The manifest describes all other
+  # files of interest, and further describes the shape of the job and
+  # the results to be run.
+  @manifest "manifest.ttl"
+
+  def start_link initial_ts do
+    Logger.info "Called Storage.Monitor.start_link (initial_ts = #{inspect initial_ts})"
+    GenStage.start_link __MODULE__, initial_ts, name: __MODULE__
+  end
+
+  def init initial_ts do
+    @doc """
+    Note that there is a single cache of UUIDs in memory, just like there
+    is a single cache of data and a single database of results.
+
+    The idea is that any given store uses randomly generated UUIDs, which
+    are de-facto unique, so there is no chance of conflict here.
+    """
+    Logger.info "Called Storage.Monitor.init (initial_ts = #{inspect initial_ts})"
+    :ets.new(:uuid, [:set, :public, :named_table])
+    { :producer, initial_ts }
+  end
+
+  def handle_demand demand, ts do
+    @doc """
+    This is the best place to put the polling for new UUIDs,
+    i.e. make a GenStage call which will poll for UUIDs, providing
+    that there the interval in question has elapsed.
+    """
+    Logger.info "Called `Storage.Monitor.handle_demand (demand = #{insd}, state = #{inss})'"
+    { :noreply, events, ts }
+  end
+  
+  def new_connection() do
+    with {:ok, token} <- Goth.Token.for_scope(@gcp_scope),
+         session      <- GCPConn.new(token.token) do
+      Logger.info "Called Storage.GCP.new_connection/0"
+      {:ok, session}
+    else
+      :error -> {:error, "Cannot obtain token/session" }
+    end
+  end
+
+  @doc """
+  Trigger the monitoring of a given GCP bucket
+  """
+  def watch_bucket(bucket_name) do
+    Logger.info("Call `Storage.GCP.watch_gcp (bucket: #{bucket_name})'")
+    with {:ok, session} <- new_connection()
+      do
+      initial_ts = DateTime.utc_now() |> DateTime.to_unix()
+      monitor_session(session, bucket_name, initial_ts, initial: true)
+    else
+      { :error, _ } = err -> err
+    end
+  end
+
+  def features_helper(%GCPObj{} = obj) do
+    Logger.info "Called `Storage.GCP.features_helper' on object #{obj.name}"
+     %{id:         obj.id,
+       bucket:     obj.bucket,
+       md5:        obj.md5Hash,
+       uri_media:  obj.mediaLink,
+       uri_self:   obj.selfLink,
+       ts_created: obj.timeCreated,
+       ts_updated: obj.updated,
+       mime_type:  obj.contentType,
+       name:       obj.name       }
+  end
+
+  @doc """
+  Given an object returned by GCP, the files of which are uploaded in the
+  following format, extract the useful parts from this well-known
+  object name.
+
+  The flat objects' names should be a string in the form
+  `<owner>/<yyyymmdd>/<uuid>/<file>', with or without a trailing slash.
+  The trailing slash conveniently tells us that it's a directory; objects
+  have `text/plain' for directories, `application/octet_stream' for empty
+  files.
+  """
+  def uuid_helper(gcp_object) do
+    Logger.info "Called `Storage.GCP.uuid_helper' on object #{gcp_object.name}"
+    with [owner, _date, uuid, res | k] <- String.split(gcp_object.name, "/"),
+	 is_directory <- length(k) > 0
+    do
+      gcp_object
+      |> Map.merge(
+	%{owner: owner, uuid: uuid, file: res, dir: is_directory}
+      )
+    else
+      [_|_] -> nil
+    end
+  end
+
+  @doc """
+  Simple wrapper around Erlang term storage table of UUIDs
+  """
+  defp ets_feasible?(uuid) do
+    case :ets.lookup(:uuid, uuid) do
+      [] -> true
+      _  ->
+	Logger.info("Job UUID #{uuid} is already running, cannot add to ETS.")
+	false
+    end
+  end
+
+  @doc """
+  Monitors the GCP objects every five minutes.
+
+  The Google GCP buckets have a fake directory structure: they're by
+  default stored in this flat object-based structure which the web
+  interface hides.
+
+  The API has several 'Folder' and 'ManagedFolder' functions but these
+  only work on buckets which have a 'hierarchical' namespace, and the
+  documentation is so bad that there does not appear to be a way to set
+  this on the web interface.
+
+  If the request is successful, cast to the check_then_run function
+  which will check the UUIDs against 1. completed jobs (an mnesia
+  database) and 2. jobs, if any, currently running.
+  """
+  def monitor_session(session, bucket_name, ts, initial \\ false) do
+    if initial do
+      Logger.info "Initial call to `GCP.Storage.monitor_session' with interval #{inspect @interval_seconds}s"
+    end
+    curr_ts = DateTime.utc_now() |> DateTime.to_unix
+    if (curr_ts - ts) < @interval_seconds and !initial do
+      monitor_session(session, bucket_name, ts)
+    else
+      with {:ok, %GCPObjs{items: objects}} <- GCPReqObjs.storage_objects_list(session, bucket_name)
+	do
+	objects
+	|> Enum.map(&features_helper/1)
+	|> Enum.map(&uuid_helper/1)
+	|> then(fn (uuids) ->
+	  GenStage.cast(
+	    __MODULE__,
+	    {:spew_uuids, :source_gcp, session, bucket_name, uuids})
+	end)
+	new_ts = DateTime.utc_now() |> DateTime.to_unix
+	monitor_session(session, bucket_name, new_ts)
+      else
+	{:error, error = %Tesla.Env{status: 401}} ->
+	  Logger.info "Query of GCP bucket #{bucket_name} appeared to time out, seek new session"
+	  # If this bit fails, we know there's really something up!
+	  {:ok, new_session} = new_connection()
+	  monitor_session(new_session, bucket_name, ts) 
+        {:error, error = %Tesla.Env{status: code, url: uri, body: msg}} ->
+	  Logger.info "Query of GCP failed with code #{code} and error message #{msg}"
+	  {:error, uri, code, msg}
+      end
+    end
+  end
+
+  defp dl_success?(obj, body) do
+    purported = obj.md5
+    actual    = :crypto.hash(:md5, body) |> Base.encode64()
+    if purported == actual do
+      { :ok, "correct", purported }
+    else
+      { :error, "incorrect", purported }
+    end  
+  end
+
+  def handle_cast({:spew_uuids, :source_gcp, session, bucket_name, uuids}, state) do
+    {:noreply, {:source_gcp, session, bucket_name, uuids}, state}
+  end
 end
