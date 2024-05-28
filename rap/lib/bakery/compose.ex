a92bac5bd4725feceab54813541061f4e78a3fcd
dguthrie <dguthrie@posteo.net>
2024-05-28 10:46:54 +0000

    Fix day of week indexing, fix HTML fragments link quoting
    FossilOrigin-Name: c822b98455559aa409935f3130cccc59d8b6b89eedabd507c658e4df683f64cc

--- rap/lib/bakery/compose.ex
+++ rap/lib/bakery/compose.ex
@@ -216,13 +216,13 @@ defmodule RAP.Bakery.Compose do
     months =   [ "January", "February", "March",
 		 "April",   "May",      "June",
 		 "July",    "August",   "September",
-		 "October", "November", "December" ]    
-    dt = unix_ts
-    |> DateTime.from_unix!()
-    |> DateTime.shift_zone!(time_zone)
+		 "October", "November", "December" ]
+    dt = unix_ts |> DateTime.from_unix!()  |> DateTime.shift_zone!(time_zone)
 
-    day_name      = weekdays |> Enum.fetch!(Date.day_of_week dt)
-    month_name    = months   |> Enum.fetch!(dt.month)
+    # These range from 1-7, 1-12 but lists are zero-indexed
+    day_name      = weekdays  |> Enum.fetch!(Date.day_of_week dt - 1)
+    month_name    = months    |> Enum.fetch!(dt.month - 1)
+    
     padded_hour   = dt.hour   |> to_string |> String.pad_leading(2, "0")
     padded_minute = dt.minute |> to_string |> String.pad_leading(2, "0") 
     
