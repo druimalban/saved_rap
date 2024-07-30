e1955ab8c8620c2a4768fe58769161b7ed04e7a3
dguthrie <dguthrie@posteo.net>
2024-07-30 16:52:22 +0000

    Call contrib/bin
    FossilOrigin-Name: 57c961bc66fd5b60a946355f31172b317584ca9b691dec71c5fc2129565cc55a

--- rap/lib/job/result.ex
+++ rap/lib/job/result.ex
@@ -129,7 +129,7 @@ defmodule RAP.Job.Result do
       # b) Guarantees about dependencies
       # We're after good reporting, and this information should certainly be part of that.
       py_result = 
- 	cmd_wrapper(python_call, "contrib/density_count_ode.py", [
+ 	cmd_wrapper(python_call, "contrib/bin/density_count_ode.py", [
  	            file_path_count,   label_count,
  	            file_path_density, label_time,  label_density])
       case py_result do
